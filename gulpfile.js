483d9f51df475ef64512a19b328fe83cee6b943f
Iaroslav <eremeev.pt@gmail.com>
2021-10-21 20:08:38 +0300

    Add gulp modules rename and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp        = require('gulp');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();;
 const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
@@ -7,7 +7,7 @@ const rename = require("gulp-rename");
 
 gulp.task('server', function() {
 
-    browserSync({
+    browserSync.init({
         server: {
             baseDir: "src"
         }
